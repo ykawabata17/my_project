e097716b5c2f93eab84c2f181dba227c811be779
ykawabata17 <AI1063@nara.kosen-ac.jp>
2022-04-21 13:40:24 +0900

    Improvement file

--- study_project/__main__.py
+++ study_project/__main__.py
@@ -1,3 +1,11 @@
+from .mylib.calc_shap import ShapImage
+from .mylib.load_data import LoadData
+from .mylib.evaluate_acc import EvaluateAcc
+
+
+def main():
+    pass
+
+
 if __name__ == '__main__':
-    from .run import main
     main()
